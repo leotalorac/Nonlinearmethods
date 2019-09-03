e9580c41a7227fc03cb331c1e381e1156096757e
leotalorac <leotalorac@unal.edu.co>
2019-09-02 22:46:42 -0500

    add display options

--- bolzano.m
+++ bolzano.m
@@ -1,14 +1,20 @@
-function y=bolzano(fun,a,b,epsilon)
+function y=bolzano(fun,a,b,epsilon,k)
+  if(k ==0)
+    disp("    K         a          b          c        F(a)       F(c)      F(b)")
+  endif
   fx = inline(fun,'x');
   c = (a+b)/2;
   fa = fx(a);
   fb = fx(b);
   fc = fx(c);
+  R = [k a c b fa fc fb];
+  disp(R);
+  k=k+1;
   if(abs(fc)<epsilon)
     y=c;
   elseif(fa*fc < 0)
-    y =(bolzano(fun,a,c,epsilon));
+    y =(bolzano(fun,a,c,epsilon,k));
   else
-    y =(bolzano(fun,c,b,epsilon));
+    y =(bolzano(fun,c,b,epsilon,k));
   endif
 endfunction
