ab5b9395a07a6e92612ae4351d730e5802818334
Luis Eduardo Otálora Cubides <leotalorac@unal.edu.co>
2019-09-04 10:41:29 -0500

    less code

--- bolzano.m
+++ bolzano.m
@@ -1,27 +1,23 @@
 function y=bolzano(fun,a,b,epsilon,maxdeep)
-  disp("    K         a          b          c        F(a)       F(c)      F(b)");
+  disp("      K               a                b                 c            F(a)            F(c)             F(b)");
   k=0;
   fx = inline(fun,'x');
   R = [];
   do
     c = (a+b)/2;
-    fc = fx(c);
-    fa = fx(a);
-    fb = fx(b);  
-    if(fa*fb >0)
+    ans = [fx(a) fx(b) fx(c)];
+    if(ans(1)*ans(2) >0)
+      c=0;
       break
     endif
     k=k+1;
-    fc = fx(c);
-    R = vertcat(R,[k a c b fa fc fb]);
-    if(fa*fc <0)
-      a = a;
+    R = vertcat(R,[k a c b ans(1) ans(3) ans(2)]);
+    if(ans(1)*ans(3) <0)
       b = c;
     else
       a = c;
-      b = b;
     endif
-  until(abs(fc)<epsilon || k==maxdeep)  
+  until(abs(ans(3))<epsilon || k==maxdeep)  
   disp(R);
   y =c;
 endfunction
